f50c67b1cf1e6e1e911dd28fcd92d7e600fb1595
sajansajan1 <sajansajan85611@gmail.com>
2026-01-06 17:35:06 +0530

    Delete app directory

--- app/product/[project_id]/page.tsx
+++
@@ -1,7 +0,0 @@
-import TechPackMakerPage from "../page";
-
-
-export default async function Page(props: { params: Promise<{ project_id: string }> }) {
-  const { project_id } = await props.params;
-  return <TechPackMakerPage projectId={project_id} />;
-}
